8042e73c4ff67029069f91a821629914e223188c
LuizFBR <luizfernando5687@gmail.com>
2025-03-18 01:29:55 -0300

    added fully functional pure PCG predicates in pcg32_pure.pl. Updated README.md and gave credits to the author.

--- README.md
+++ README.md
@@ -1,10 +1,60 @@
 # Relational-Randomness
-Repo for storing relational predicates for give me a organized way to plan my work for implementing Pseudo-Random Number Generation (PRNG) and Random Sampling; Distribution Algorithms.
 
-## Potential issues:
+Purely relational predicates that implement Pseudo-Random Number Generation (PRNG) and Random Sampling (to-do).
 
-A lot of the algorithms I found seem to implement the right bit shift operator, with some very specific values, but, as we all know, right bit shifting can map a lot of different values to 0, e.g., 5 >> 3 = 0, 3 >> 3 = 0, 1 >> 3 = 0, ...
-So if you start with your super specific value one way, when going backwards you could generate a not so specific value the other way ...
-Which means the statistical qualities we so love and want could be completely decimated by left directional queries. 
-The question is, does the reversibility keep the statistical properties that this imperative algorithm guarantees for good random values generation?
-If you use it right directionally it should work just fine, though.
+##### To-do
+
+- Robust PRNG tests;
+- Sampling algorithms;
+- Noise generating algorithms;
+- Implement other PRNGs (Mersenne Twister, LCG, etc...);
+- DCG argument passing.
+
+## About
+
+This repo provides the following predicates:
+
+| Predicate          | Description |
+| ------------------ | ----------- |
+| random_generator/1 | A pre-built random generator |
+| random_generator/3 | Random generator with a intial seed and sequence. |
+| random/3           | Random number from 0 to 2^32-1 |
+| random_bounded/4   | Random number from 0 to Max-1 |
+| random_between/5   | Random number from Min to Max-1 (Max - Min < 2^32) |
+| random_list/4      | List of N random numbers from 0 to 2^32-1 |
+| random_bounded/4   | List of N random numbers from 0 to Max-1 |
+| random_between/5   | List of N random numbers from Min to Max-1 (Max - Min < 2^32) |
+| probability/4      | Reifies the truth value of Probability/Precision chance of an event happening into T |
+
+## How to use
+
+## PCG
+
+Each predicate (except random_generator/1 ) provides the two initial arguments RandomGenerator and NextRandomGenerator, e.g.:
+
+```prolog
+random(Pcg32randomt, NextPcg32randomt, RandomInt) :-
+    pcg32_random_r(Pcg32randomt, NextPcg32randomt, RandomInt).
+```
+
+And to use them you need an instance of a random generator and plug it into the first argument then into the second argument, like so:
+
+```Prolog
+?- random_generator(X),random(X,X1,N1), random_bounded(X1,X2,5,N2), random_betweenlist(X2,X3,-5,10,5,L1). 
+X = pcg32randomt(9600629759793949339, 15726070495360670683),
+X1 = pcg32randomt(14425053563923168794, 15726070495360670683),
+N1 = 355248013,
+X2 = pcg32randomt(13821270098544127085, 15726070495360670683),
+N2 = 0,
+X3 = pcg32randomt(2638482799351248520, 15726070495360670683),
+L1 = [5, 5, -1, 3, -4].
+```
+
+## Credits
+
+Credits for the PCG algorithms goes to:
+
+PCG: A Family of Simple Fast Space-Efficient Statistically Good Algorithms for Random Number Generation
+Author: "Melissa E. O'Neill"
+
+PCG implemenation based on https://github.com/imneme/pcg-c-basic/blob/master/pcg_basic.c
---
+++ prolog/pcg32_pure.pl
@@ -0,0 +1,122 @@
+:- module(pcg32_pure,
+[
+    random_generator/1,
+    random_generator/3,
+    random/3,
+    random_bounded/4,
+    random_between/5,
+    random_list/4,
+    random_boundedlist/5,
+    random_betweenlist/6,
+    probability/4
+]).
+
+:- use_module(library(reif)).
+:- use_module(library(clpfd)).
+:- set_prolog_flag(clpfd_monotonic, true).
+
+% reference Minimal C Implementation: https://www.pcg-random.org/download.html
+% PCG: A Family of Simple Fast Space-Efficient Statistically Good Algorithms for Random Number Generation
+% Author: "Melissa E. O'Neill"
+% based on https://github.com/imneme/pcg-c-basic/blob/master/pcg_basic.c
+
+% typedef struct { uint64_t state;  uint64_t inc; } pcg32_random_t;
+% pcg32randomt(State,Inc).
+
+% -------------------- public predicates ---------------------------:
+
+% A default random number generator.
+random_generator(pcg32randomt(0x853c49e6748fea9b, 0xda3e39cb94b95bdb)).
+
+random_generator(InitState, InitSeq, Pcg32randomt) :-
+    pcg32_srandom_r(InitState, InitSeq, Pcg32randomt).
+
+% Random integer in [0,2^32-1].
+random(Pcg32randomt, NextPcg32randomt, RandomInt) :-
+    pcg32_random_r(Pcg32randomt, NextPcg32randomt, RandomInt).
+
+% Random integer in [0,Max-1].
+random_bounded(Pcg32randomt, NextPcg32randomt, Max, RandomInt) :-
+    pcg32_boundedrand_r(Pcg32randomt, NextPcg32randomt, Max, RandomInt).
+
+% Random integer in [Min,Max].
+random_between(Pcg32randomt, NextPcg32randomt, Min, Max, RandomInt) :-
+    #(Max) - #(Min) #< 2^32, #(Min) #< #(Max),
+    #(Bound) #= #(Max) - #(Min),
+    pcg32_boundedrand_r(Pcg32randomt,NextPcg32randomt,Bound,R),
+    #(RandomInt) #= #(R) + #(Min).
+
+% Relationship between a natural number N and a list of random numbers L such that length(L,N).
+random_list(Pcg32randomt, FinalPcg32randomt, N, RL) :-
+    #(N) #> 0 #<==> #(T),
+    if_(   T = 0
+       , ( Pcg32randomt = FinalPcg32randomt, RL = [] )
+       , ( #(N1) #= #(N) - 1,
+           pcg32_random_r(Pcg32randomt, NextPcg32randomt, RandomInt),
+           RL = [RandomInt|RandomInts],
+           random_list(NextPcg32randomt, FinalPcg32randomt, N1, RandomInts)
+         )
+       ).
+
+% Relationship between a natural number N and a list of random bounded numbers L such that length(L,N).
+random_boundedlist(Pcg32randomt, FinalPcg32randomt, Bound, N, RL) :-
+    #(N) #> 0 #<==> #(T),
+    if_(   T = 0
+       , ( Pcg32randomt = FinalPcg32randomt, RL = [] )
+       , ( #(N1) #= #(N) - 1,
+           pcg32_boundedrand_r(Pcg32randomt, NextPcg32randomt, Bound, RandomInt),
+           RL = [RandomInt|RandomInts],
+           random_boundedlist(NextPcg32randomt, FinalPcg32randomt, Bound, N1, RandomInts)
+         )
+       ).
+
+% Relationship between a natural number N and a list of random numbers between Min and Max) L such that length(L,N).
+random_betweenlist(Pcg32randomt, FinalPcg32randomt, Min, Max, N, RL) :-
+    #(N) #> 0 #<==> #(T),
+    if_(   T = 0
+       , ( Pcg32randomt = FinalPcg32randomt, RL = [] )
+       , ( #(N1) #= #(N) - 1,
+           random_between(Pcg32randomt, NextPcg32randomt, Min, Max, RandomInt),
+           RL = [RandomInt|RandomInts],
+           random_betweenlist(NextPcg32randomt, FinalPcg32randomt, Min, Max, N1, RandomInts)
+         )
+       ).
+
+% T is reified - if an event with probability Probability/Precision happens, T = true, else T = false.
+probability(Pcg32randomt, NextPcg32randomt, Probability/Precision, T) :-
+    pcg32_boundedrand_r(Pcg32randomt, NextPcg32randomt, Precision, RandomNumber),
+    #(RandomNumber) #=< #(Probability) #<==> #(T).
+
+% -------------------- private predicates ---------------------------:
+
+% Random Number Generator
+% pcg32randomt_initstate_initseq_newpcg32randomt(Pcg32randomt,InitState,InitSeq,NextPcg32randomt) :-
+pcg32_srandom_r( InitState, InitSeq, FinalPcg32randomt) :-
+    Sup is 2^64 - 1, InitState in 0..Sup, InitSeq in 0..Sup,
+    1 #= #(InitSeq) /\ 1,
+    #(Inc) #= ( ( #(InitSeq) << 1) \/ 1 ) mod Sup,
+    pcg32_random_r(pcg32randomt(0,Inc), pcg32randomt(S2,Inc), _),
+    #(S3) #= ( #(S2) + #(InitState) ) mod Sup,
+    pcg32_random_r( pcg32randomt(S3,Inc), FinalPcg32randomt, _).
+
+% Uniformly distributed 32-bit random number:
+% pcg32_random_r(+Pcg32randomt, -NextState, -RandomValue)
+pcg32_random_r( pcg32randomt(OldState,Inc), pcg32randomt(NextState,Inc), RandomValue ) :-
+    #(NextState) #= ( ( #(OldState) * 6364136223846793005) + (#(Inc) \/ 1) ) mod (2^64),
+    #(XorShifted) #=( ( ( #(OldState) >> 18) xor #(OldState)) >> 27) mod (2^32), % here and in the line below mod 2^32 might be unnecessary, but i'm using it for precaution.
+    #(Rot) #= ( #(OldState) >> 59) mod (2^32),
+    #(RandomValue) #= ( (#(XorShifted) >> #(Rot)) \/ (#(XorShifted) << (- #(Rot) /\ 31)) ) mod (2^32).
+
+% Bounded random number using rejection sampling
+pcg32_boundedrand_r( Pcg32randomt, NextPcg32randomt, Bound, Result) :-
+    Sup is 2^32, Bound in 1..Sup,
+    bounded_sample( Pcg32randomt, NextPcg32randomt, Bound, Result).
+
+% Rejection sampling:
+bounded_sample(Pcg32randomt, FinalPcg32randomt, Bound, Result) :-
+    pcg32_random_r(Pcg32randomt, NextPcg32randomt, R),
+    #(R) #>= 2^32 mod #(Bound) #<==> #(T), % had to remove -bound mod bound optimization, since it only worked for 32 bits representations.
+    if_(   T = 1
+       , ( #(Result) #= #(R) mod #(Bound), FinalPcg32randomt = NextPcg32randomt )
+       ,   bounded_sample(NextPcg32randomt, FinalPcg32randomt, Bound, Result)
+    ).
